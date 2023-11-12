4198f84e6827f96b38312b2f1b2759253d2345e2
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2023-11-11 17:34:20 -0700

    Update: tests to confirm logged in user, go to bookstore, go to book details page and add book

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class ProfilePage {
     // declaring the webdriver variable
